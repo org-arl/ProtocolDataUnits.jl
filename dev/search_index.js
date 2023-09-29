025c6e73737f6229dbc23ccec2ae4181de34d854
Documenter.jl <documenter@juliadocs.github.io>
2023-09-29 14:12:51 +0000

    build based on 8f371b9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#PDUs.jl","page":"Home","title":"PDUs.jl","text":"","category":"section"},{"location":"index.html","page":"Home","title":"Home","text":"CurrentModule = PDUs","category":"page"},{"location":"index.html","page":"Home","title":"Home","text":"TODO","category":"page"}]
+}
